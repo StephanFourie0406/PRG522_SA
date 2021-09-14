e8d782cbeace2a8174c275e8514c512830900417
Stephan Fourie <stephanfourie0406@gmail.com>
2021-09-14 21:26:52 +0200

    Add project files.

---
+++ PRG522_SA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRG522_SA.MvcApplication" Language="C#" %>
---
+++ PRG522_SA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRG522_SA.Startup))]
+namespace PRG522_SA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
